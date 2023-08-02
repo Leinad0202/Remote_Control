be00b8e73e85c5123c93fd765b0658dc852d7f66
Leinad0202 <104594870+Leinad0202@users.noreply.github.com>
2023-08-02 18:07:16 -0300

    Teste

---
+++ src/main/java/com/mycompany/aula_06/AULA_06.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.aula_06;
+
+public class AULA_06 {
+
+    public static void main(String[] args) {
+        
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
